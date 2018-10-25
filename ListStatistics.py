4f91df448830fdafb74533a83b4f751585e3c6e8
John Goodleaf <jgoodleaf@hamotoconsulting.com>
2018-10-25 10:32:50 -0700

    Notes from John

--- ListStatistics.py
+++ ListStatistics.py
@@ -1,28 +1,44 @@
 #Statistics on lists of numbers
 import statistics
 import sys
-max = -sys.maxsize -1
-min = sys.maxsize
-sum = 0
-l1 = []
-while True:
-    x = input("Enter number : ")
-    if x == 'Q' :
-        break
-    l1.append (int(x))
-    if int(x) > max :
-        max=int(x)
-    if int(x) < min :
-        min=int(x)
-    sum = sum + int(x)    
-print("Here is the list you entered : ")
-print(l1)
-mean =  statistics.mean(l1) 
-print("Mean is : %s" %mean)
-median = statistics.median(l1)
-print("Median is : %s" %median)
-print ("Maximum number is %s" %max)
-print ("Minimum number is %s" %min)
-print ("Sum of list is %s" %sum)
 
-    
+# sum, and max are built-in functions
+# and you should avoid using variable
+# names that clash with the names
+# of functions
+
+# max = -sys.maxsize -1
+# min = sys.maxsize
+# sum = 0
+# l1 = []
+
+number_list = []
+
+while True:
+    x = input("Enter number or \'Q\' if you're done adding numbers : ")
+    if x.upper() == 'Q' :
+        break
+    try:
+        number_list.append(int(x))
+    except ValueError:
+        print("You didn't give me a number or Q")
+        continue # this forces the next loop early
+    # l1.append (int(x))
+    # if int(x) > max :
+    #     max=int(x)
+    # if int(x) < min :
+    #     min=int(x)
+    # sum = sum + int(x)    
+print(f"Here is the list you entered : {number_list}")
+#print(l1)
+#mean =  statistics.mean(l1) 
+print(f"Mean is : {statistics.mean(number_list)}")
+#median = statistics.median(l1)
+print(f"Median is : {statistics.median(number_list)}")
+print (f"Maximum number is {max(number_list)}")
+print (f"Minimum number is {min(number_list)}")
+print (f"Sum of list is {sum(number_list)}")
+
+# See how the program became shorter and a little 
+# neater? I used "f" strings and the built-in 
+# functions
--- RandomNumerGuesser.py
+++ RandomNumerGuesser.py
@@ -19,3 +19,31 @@ while True :
         print ("You are very close, try again") 
     ch = ch + 1
 print ("You took %s chances to guess" %ch)
+
+# import random
+
+# secret_number = random.choice(range(101)) # this is slightly more random on many OSs
+
+# guess_counter = 0
+
+# while True:
+#     guess = input("Enter a number or type \'q\' to quit\t")
+#     print(guess)
+#     if guess == 'q':
+#         break
+#     else:
+#         try:
+#             my_guess = int(guess)
+#             guess_counter += 1
+#         except:
+#             print("That's not an Integer! (Whole Number)")
+#             continue
+    
+#     if my_guess == secret_number:
+#         print("You Win!")
+#         print("It took {0} guesses".format(guess_counter))
+#         break
+#     elif my_guess < secret_number:
+#         print("Too low... Try a larger number")
+#     elif my_guess > secret_number:
+#         print("Too high... Try a smaller number")
